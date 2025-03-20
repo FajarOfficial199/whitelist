69252787f03a167366df1cbd770cedd6927fa0b0
FajarOfficial199 <fajarshidik709@gmail.com>
2025-03-20 23:21:26 +0700

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,22 @@
+module.exports = {
+    namaserver: "Ventexas Roleplay",
+
+    // Konfigurasi Email
+    emailHost: "",
+    emailPort: 465,
+    emailUser: "gmail",
+    emailPass: "xold obyt kxsu whgs",
+
+    // Konfigurasi SFTP
+    host: "node.rumahh.biz.id",
+    port: 2022,
+    username: "fajar.d5755e5f",
+    password: "12",
+    sftppath: "/scriptfiles/WhiteList/",
+    namafile: ".txt",
+    MONGODB_URL: "mongodb+srv://fajarshidik709:5xDc3VO36uCBZq5N@cluster0.qqz27.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+
+    // Konfigurasi Cloudflare Turnstile
+    cloudflareSiteKey: "YOUR_CLOUDFLARE_SITE_KEY",
+    cloudflareSecret: "YOUR_CLOUDFLARE_SECRET_KEY",
+};
